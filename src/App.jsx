6094f56fccad474f83ad5e547c08e7d881bde0b6
MatiasBebczuk <48242936@est.ort.edu.ar>
2025-06-24 08:25:11 -0300

    Usar estructura de carpetas pedida

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Navbar from "./Navbar";
+import Layout from "./Layouts/Layout";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/TP5_EFSI2-Bebczuk_Susel/" element={<Navbar />}>
+        <Route path="/TP5_EFSI2-Bebczuk_Susel/" element={<Layout />}>
           <Route index element={<Home />} />
           {/* <Route path="/TP5_EFSI2-Bebczuk_Susel/ruta1" element={<componente />} /> */}
         </Route>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "./Footer.css"
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; TP07 inc</p>
+        </footer>
+    );
+}
+
+export default Footer;
