66cff3cb577f187087a735ddabfbc4b532e15113
MatiasBebczuk <48242936@est.ort.edu.ar>
2025-06-24 08:14:24 -0300

    Crear plantilla de proyecto

---
+++ src/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Outlet, Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar(){
+  return (
+    <>
+      <nav>
+        <ul>
+          <li><Link to="/TP5_EFSI2-Bebczuk_Susel/">Home</Link></li>
+          {/* <li><Link to="/TP5_EFSI2-Bebczuk_Susel/p1">Nuevas reservas</Link></li>
+          <li><Link to="/TP5_EFSI2-Bebczuk_Susel/MisCitas">Mis citas</Link></li> */}
+        </ul>
+      </nav>      
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+};
+
+export default Navbar;
