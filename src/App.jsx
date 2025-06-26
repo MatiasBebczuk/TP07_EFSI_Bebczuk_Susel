7605828a40c07132d697db1a3c1588c14b69169c
gonzalosusel <48589225@est.ort.edu.ar>
2025-06-26 08:15:42 -0300

    Terminar página categoría

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Contacto from "./pages/Contacto";
 import Productos from "./pages/Productos";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductoDetalle from './pages/ProductoDetalle';
+import Categoria from "./pages/Categoria";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
@@ -16,6 +18,8 @@ export default function App() {
           <Route path="/TP7_EFSI_Bebczuk_Susel/Contacto" element={<Contacto />} />
           <Route path="/TP7_EFSI_Bebczuk_Susel/Productos" element={<Productos />} />
           <Route path="/TP7_EFSI_Bebczuk_Susel/ProductoDetalle/:IdProducto" element={<ProductoDetalle />} />
+          <Route path="/TP7_EFSI_Bebczuk_Susel/Categoria/:IdCategoria" element={<Categoria />} />
+          <Route path="/TP7_EFSI_Bebczuk_Susel/*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
