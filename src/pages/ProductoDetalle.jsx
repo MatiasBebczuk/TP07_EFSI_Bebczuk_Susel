fc386be3e8d89f2283da5d8894d5221afab7c17e
gonzalosusel <48589225@est.ort.edu.ar>
2025-07-01 08:42:39 -0300

    Terminado

--- src/pages/ProductoDetalle.jsx
+++ src/pages/ProductoDetalle.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Fragment } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import "./ProductoDetalle.css";
@@ -25,7 +25,12 @@ export default function ProductoDetalle(){
                 <h2>Opiniones</h2>
                 {
                     producto.reviews.map((review, i) => <div className="review" key={i}>
-                        <h3>{review.reviewerName} | {review.rating}/5</h3>
+                        <div style={{display: "flex", gap: 5, alignItems: "center"}}>
+                            <h3>{review.reviewerName} | {review.rating}/5</h3>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-star-fill" viewBox="0 0 16 16">
+                                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                        </div>
                         <p>{review.comment}</p>
                     </div>)
                 }
